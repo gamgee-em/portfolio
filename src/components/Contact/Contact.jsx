032682721a0d4fb01027bff258a3c90f976ae7d6
Sam Sweigart <samuel.sweigart@gmail.com>
2022-08-28 13:04:39 -0400

    added contact modal enter/exit animations

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import './Contact.css';
 
 const Contact = () => {
@@ -7,49 +8,79 @@ const Contact = () => {
   const handleShowContact = () => () =>
     showContact ? setShowContact(false) : setShowContact(true);
 
+  const contactModalVariants = {
+    initial: {
+      opacity: 0,
+      scale: .1,
+      y: '35vh',
+      x: '40vw',
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      x: 0,
+     
+    },
+    exit: {
+      opacity: 0,
+      scale: .1,
+      y: '35vh',
+      x: '40vw',
+    },
+    
+  };
+
   return (
     <>
       <button className='contact-btn' onClick={handleShowContact()}>
         Contact
       </button>
+      <AnimatePresence exitBeforeEnter={true}>
+        {showContact && (
+          <motion.section
+            className='contact'
+            variants={contactModalVariants}
+            initial='initial'
+            animate='visible'
+            exit='exit'
+          >
+            <h2>Get in touch...</h2>
+            <input
+              className='fname'
+              type='text'
+              name='fname'
+              placeholder='First Name'
+            />
+            <input
+              className='lname'
+              type='text'
+              name='lname'
+              placeholder='Last Name'
+            />
+            <input
+              className='email'
+              type='text'
+              name='email'
+              placeholder='Your E-mail'
+            />
+            <input
+              className='subject'
+              type='text'
+              name='subject'
+              placeholder='Subject'
+            />
 
-      {showContact && (
-        <section className='contact'>
-          <h2>Get in touch...</h2>
-          <input
-            className='fname'
-            type='text'
-            name='fname'
-            placeholder='First Name'
-          />
-          <input
-            className='lname'
-            type='text'
-            name='lname'
-            placeholder='Last Name'
-          />
-          <input
-            className='email'
-            type='text'
-            name='email'
-            placeholder='Your E-mail'
-          />
-          <input
-            className='subject'
-            type='text'
-            name='subject'
-            placeholder='Subject'
-          />
-
-          <textarea
-            className='message'
-            type='text'
-            name='message'
-            placeholder='Type your message here... 📬'
-          />
-          <button className='submit-btn'> Send </button>
-        </section>
-      )}
+            <textarea
+              className='message'
+              type='text'
+              name='message'
+              placeholder='Type your message here... 📬'
+            />
+            <button className='submit-btn'> Send </button>
+          </motion.section>
+        )}
+      </AnimatePresence>
     </>
   );
 };
